257ddd01da5f65eee613b9be905e08058a8eca24
Jonathan Tetry <jonathantetry2309@gmail.com>
2026-08-21 11:31:37 -0400

    added a function to do forward on the network, cannot figure out a solution to allow the function to be jit compiled and also allow for non hard-coded network layers

--- pettingzoo/chessrl.py
+++ pettingzoo/chessrl.py
@@ -1,5 +1,7 @@
 from pettingzoo import make
-from jax import random, Array
+import jax
+import time
+from jax import random, Array, lax, nn
 import jax.numpy as jnp
 
 env = make("aec", "classic/chess-v6")
@@ -11,6 +13,8 @@ print(*(f"Act space for {i}: {env.action_space(i)}\n" for i in env.agents))
 
 OBSERVATION_SIZE = 8 * 8 * 111
 ACTION_SIZE = 4672
+ACTOR_LAYER_SIZES = [OBSERVATION_SIZE, 256, 256, ACTION_SIZE]
+CRITIC_LAYER_SIZES = [OBSERVATION_SIZE, 256, 256, 1]
 
 key = random.PRNGKey(67)
 
@@ -27,3 +31,32 @@ def initialize_parameters(key : Array, actor_layer_sizes : list[int], critic_lay
         critic_parameters[f"w{i}"] = random.normal(critic_keys[i], shape=(critic_layer_sizes[i], critic_layer_sizes[i+1]), dtype=jnp.float32) * 0.01
         critic_parameters[f"b{i}"] = jnp.zeros((critic_layer_sizes[i+1],), dtype=jnp.float32)
     return actor_parameters, critic_parameters
+
+@jax.jit
+def network_forward(parameters : dict, observation : Array) -> Array: #for now uses hard-coded params, idk how to jit it while keeping variable model size
+    # num_layers = len(parameters) // 2
+
+    # def compute_layer(i : int, x : Array) -> Array:
+    #     x = jnp.dot(x, parameters[f"w{i}"]) + parameters[f"b{i}"]
+    #     x = jnp.where(i < num_layers - 1, nn.relu(x), x)
+    #     return x, None
+
+    # x = observation.reshape(-1)
+    # x = lax.fori_loop(0, num_layers, compute_layer, x)
+    # return x
+    x = observation.reshape(-1)
+    x = nn.relu(jnp.dot(x, parameters["w0"]) + parameters["b0"])
+    x = nn.relu(jnp.dot(x, parameters["w1"]) + parameters["b1"])
+    x = jnp.dot(x, parameters["w2"]) + parameters["b2"]
+    return x
+
+if __name__ == "__main__":
+    
+    actor_parameters, critic_parameters = initialize_parameters(key, ACTOR_LAYER_SIZES, CRITIC_LAYER_SIZES)
+    # print(actor_parameters)
+    # print(critic_parameters)
+    sample_observation = env.observe(env.agents[0])["observation"]
+    print(network_forward(critic_parameters, sample_observation))
+    start = time.time()
+    result = network_forward(critic_parameters, sample_observation)
+    print(time.time() - start)
