d0e29d1a5953c84b4fc85f2e75e8b44ec9c7374e
Jonathan Tetry <jonathantetry2309@gmail.com>
2026-08-21 13:05:08 -0400

    created dataclasses for parameters and for timesteps. also created collect_trajectories function that can collect t timesteps of rollout data (with masking for legal moves)

--- pettingzoo/chessrl.py
+++ pettingzoo/chessrl.py
@@ -1,8 +1,8 @@
 from pettingzoo import make
-import jax
 import time
-from jax import random, Array, lax, nn
-import jax.numpy as jnp
+import jax
+from jax import random, Array, lax, nn, numpy as jnp
+from dataclasses import dataclass
 
 env = make("aec", "classic/chess-v6")
 env.reset()
@@ -18,7 +18,22 @@ CRITIC_LAYER_SIZES = [OBSERVATION_SIZE, 256, 256, 1]
 
 key = random.PRNGKey(67)
 
-def initialize_parameters(key : Array, actor_layer_sizes : list[int], critic_layer_sizes : list[int]) -> tuple[dict[str, Array], dict[str, Array]]:
+@dataclass
+class Timestep:
+    obs : Array
+    action : int
+    log_prob : float
+    reward : float
+    value : float
+    agent : str
+    done : bool
+
+@dataclass
+class Parameters:
+    actor : dict[str, Array]
+    critic : dict[str, Array]
+
+def initialize_parameters(key : Array, actor_layer_sizes : list[int], critic_layer_sizes : list[int]) -> Parameters:
     actor_key, critic_key = random.split(key, 2)
     actor_keys = random.split(actor_key, len(actor_layer_sizes) - 1)
     critic_keys = random.split(critic_key, len(critic_layer_sizes) - 1)
@@ -30,7 +45,7 @@ def initialize_parameters(key : Array, actor_layer_sizes : list[int], critic_lay
     for i in range(len(critic_layer_sizes) - 1):
         critic_parameters[f"w{i}"] = random.normal(critic_keys[i], shape=(critic_layer_sizes[i], critic_layer_sizes[i+1]), dtype=jnp.float32) * 0.01
         critic_parameters[f"b{i}"] = jnp.zeros((critic_layer_sizes[i+1],), dtype=jnp.float32)
-    return actor_parameters, critic_parameters
+    return Parameters(actor=actor_parameters, critic=critic_parameters)
 
 @jax.jit
 def network_forward(parameters : dict, observation : Array) -> Array: #for now uses hard-coded params, idk how to jit it while keeping variable model size
@@ -43,20 +58,55 @@ def network_forward(parameters : dict, observation : Array) -> Array: #for now u
 
     # x = observation.reshape(-1)
     # x = lax.fori_loop(0, num_layers, compute_layer, x)
-    # return x
+    # return 
+    
     x = observation.reshape(-1)
     x = nn.relu(jnp.dot(x, parameters["w0"]) + parameters["b0"])
     x = nn.relu(jnp.dot(x, parameters["w1"]) + parameters["b1"])
     x = jnp.dot(x, parameters["w2"]) + parameters["b2"]
     return x
 
+def collect_trajectories(key : Array, timesteps : int, parameters_0 : Parameters, parameters_1 : Parameters) -> list[Timestep]:
+    trajectories = []
+    keys = random.split(key, timesteps)
+    parameters = {
+        env.agents[0] : parameters_0,
+        env.agents[1] : parameters_1,
+    }
+    env.reset()
+    while timesteps > 0:
+        for agent in env.agent_iter():
+            timesteps -= 1
+            observation, reward, termination, truncation, info = env.last()
+            actor_logits = network_forward(parameters[agent].actor, observation["observation"])
+            masked_actor_logits = jnp.where(observation["action_mask"] == 1, actor_logits, -jnp.inf)
+            masked_log_probs = nn.log_softmax(masked_actor_logits)
+            value = network_forward(parameters[agent].critic, observation["observation"])
+            action_index = int(random.categorical(keys[timesteps], masked_actor_logits))
+            trajectories.append(Timestep(obs=observation, 
+                                        reward=reward, 
+                                        log_prob=masked_log_probs[action_index], 
+                                        action=action_index, 
+                                        value=value, 
+                                        agent=agent, 
+                                        done=(termination or truncation)))
+            if truncation or termination or timesteps <= 0:
+                break
+            env.step(action_index)
+        env.reset()
+    return trajectories
+
 if __name__ == "__main__":
-    
-    actor_parameters, critic_parameters = initialize_parameters(key, ACTOR_LAYER_SIZES, CRITIC_LAYER_SIZES)
+    k1, k2 = random.split(key, 2)
+    p0 = initialize_parameters(k1, ACTOR_LAYER_SIZES, CRITIC_LAYER_SIZES)
+    p1 = initialize_parameters(k2, ACTOR_LAYER_SIZES, CRITIC_LAYER_SIZES)
     # print(actor_parameters)
     # print(critic_parameters)
     sample_observation = env.observe(env.agents[0])["observation"]
-    print(network_forward(critic_parameters, sample_observation))
+    print(network_forward(p0.critic, sample_observation))
     start = time.time()
-    result = network_forward(critic_parameters, sample_observation)
+    result = network_forward(p0.critic, sample_observation)
     print(time.time() - start)
+    rollout = collect_trajectories(key, 2048, p0, p1)
+  #  print(rollout)
+    print("ROLLOUT SIZE:", len(rollout))
